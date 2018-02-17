b009aa83d141be2f7c4ed58391c9c7c0723c6a78
Daniel Diaz Medrano <danieldiaz129@gmail.com>
2018-02-17 12:52:35 -0800

    Created links to pages.

---
+++ scripts/contact.js
@@ -0,0 +1,6 @@
+let x = document.getElementById('contact_me_form');
+var createform = document.createElement('form'); // Create New Element Form
+x.appendChild(createform);
+
+
+$('contact_me_form').on_submit('')
